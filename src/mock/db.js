a1a3cfe1a26fd7b10cdb15ed6dc6f5463c5d971d
xuyq <xuyq@hnao.cn>
2020-08-07 10:13:33 +0800

    update

---
+++ src/mock/db.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const mockData = {
+  DB: {},
+  readDir(path) {
+    const stats = fs.statSync(path);
+    if (stats.isDirectory()) {
+      const files = fs.readdirSync(path);
+      files.forEach(file => {
+        this.readDir(`${path}/${file}`);
+      });
+    } else {
+      const data = fs.readFileSync(path);
+      // 把json文件的路径作为key
+      const key = path.replace('.json', '').replace(/\//g, '_');
+      this.DB[key] = JSON.parse(data)
+    }
+  }
+};
+mockData.readDir('data')
+module.exports = mockData.DB
--- src/mock/index.js
+++ src/mock/index.js
@@ -4,12 +4,22 @@
 
 // /service/attrCode/paged
 // console.log(Mock)
-const path = require('path')
+
 const jsonServer = require('json-server')
+const db = require('./db.js');
 const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const router = jsonServer.router(db)
 const middlewares = jsonServer.defaults()
 server.use(middlewares)
+server.use(jsonServer.bodyParser)
+
+// 拦截客户端请求,进行自定义处理
+server.use((req, res, next) => {
+  // 手动映射,更改请求url(/steps/step1 => /steps_step1)
+  req.url = req.url.replace(/\//g, '_').replace('_', '/');
+  next();
+})
+
 server.use(router)
 server.listen(3004, () => {
   console.log('JSON Server is running')
